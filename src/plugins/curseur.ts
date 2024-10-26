f09035ec59f05922a6d45f79be6f80ec15bee264
Timur <toombez.timurtokaev@gmail.com>
2024-10-27 02:12:10 +0300

    Provide composable as global

--- src/plugins/curseur.ts
+++ src/plugins/curseur.ts
@@ -1,7 +1,16 @@
 import type { Plugin } from "vue"
+import { useCurseur } from "@/composables/useCurseur"
+
+declare module 'vue' {
+    interface ComponentCustomProperties {
+        $curseur: ReturnType<typeof useCurseur>;
+    }
+}
 
 interface PluginOptions {}
 
-const curseurPlugin: Plugin<PluginOptions> = (app, options) => {}
+const curseurPlugin: Plugin<PluginOptions> = (app, options) => {
+    app.config.globalProperties.$curseur = useCurseur()
+}
 
 export default curseurPlugin
