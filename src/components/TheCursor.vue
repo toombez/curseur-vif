b0fe6ad8bfc133fa4eb915a29bbf4736854db34d
Timur <toombez.timurtokaev@gmail.com>
2024-10-27 02:19:35 +0300

    Add TheCursor

---
+++ src/components/TheCursor.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useCurseur } from '@/composables/useCurseur'
+import { computed } from 'vue'
+
+const {
+    cursorX,
+    cursorY,
+} = useCurseur()
+
+const translateStyles = computed(() => {
+    const translateX = `calc(${cursorX.value}px - 50%)`
+    const translateY = `calc(${cursorY.value}px - 50%)`
+
+    return `${translateX} ${translateY}`
+})
+</script>
+
+<template>
+    <div
+        v-if="$curseur.isCursorVisible"
+        :style="{ translate: translateStyles }"
+        :class="$style.cursor"
+    >
+        {{ cursorX }} | {{ cursorY }}
+    </div>
+</template>
+
+<style module>
+.cursor {
+    position: fixed;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+    z-index: 9999;
+}
+</style>
