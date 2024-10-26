ea472fa15542619026206a601360bc7dfdd12d08
Timur <toombez.timurtokaev@gmail.com>
2024-10-27 02:00:25 +0300

    Add useCursor

---
+++ src/composables/useCursor.ts
@@ -0,0 +1,34 @@
+import { useMouse, useToggle } from "@vueuse/core"
+import { useDynamicComponent } from "./useDynamicComponent"
+import { shallowReadonly } from "vue"
+
+const useCursor = () => {
+    const {
+        attrs,
+        component,
+        isHaveDynamicComponent,
+        deleteDynamicComponent,
+        setDynamicComponent,
+    } = useDynamicComponent()
+
+    const [isCursorVisible, toggleCursorVisibility] = useToggle()
+
+    const { x: cursorX, y: cursorY } = useMouse({
+        touch: false,
+        type: 'client',
+    })
+
+    return {
+        CursorComponent: component,
+        cursorAttrs: attrs,
+        cursorX: shallowReadonly(cursorX),
+        cursorY: shallowReadonly(cursorY),
+        isCursorVisible: shallowReadonly(isCursorVisible),
+        isHaveCursorComponent: isHaveDynamicComponent,
+        toggleCursorVisibility,
+        deleteCursorComponent: deleteDynamicComponent,
+        setCursorComponent: setDynamicComponent,
+    }
+}
+
+export { useCursor }
