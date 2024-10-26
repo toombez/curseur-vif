60a70b070fd82defb5165be74c742c7c54519915
Timur <toombez.timurtokaev@gmail.com>
2024-10-27 01:29:15 +0300

    Init plugin

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import curseurPlugin from './plugins/curseur'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(curseurPlugin, {})
+    .mount('#app')
---
+++ src/plugins/curseur.ts
@@ -0,0 +1,7 @@
+import type { Plugin } from "vue"
+
+interface PluginOptions {}
+
+const curseurPlugin: Plugin<PluginOptions> = (app, options) => {}
+
+export default curseurPlugin
