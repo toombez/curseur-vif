82b2ee9d185a7eb16b71f0274062da140456a65c
Timur <toombez.timurtokaev@gmail.com>
2024-10-28 13:40:58 +0300

    Provide cursor-hover directive

--- src/plugins/curseur.ts
+++ src/plugins/curseur.ts
@@ -1,12 +1,15 @@
 import type { Plugin } from "vue"
 import { useCurseur } from "@/composables/useCurseur"
 import { vCursorDirective } from "@/directives/vCursor"
+import { vCursorHoverDirective } from "@/directives/vCursorHover";
 
 declare module 'vue' {
     interface ComponentCustomProperties {
         $curseur: ReturnType<typeof useCurseur>;
 
         vCursor: typeof vCursorDirective
+
+        vCursorHover: typeof vCursorHoverDirective
     }
 }
 
@@ -16,6 +19,7 @@ const curseurPlugin: Plugin<PluginOptions> = (app, options) => {
     app.config.globalProperties.$curseur = useCurseur()
 
     app.directive('cursor', vCursorDirective)
+    app.directive('cursor-hover', vCursorHoverDirective)
 }
 
 export default curseurPlugin
