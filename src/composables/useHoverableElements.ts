a2f25dac89fac0780434642df598cac32982ac38
Timur <toombez.timurtokaev@gmail.com>
2024-10-28 13:38:53 +0300

    Add useHoverableElements

--- src/composables/useHoverableElements.ts
+++ src/composables/useHoverableElements.ts
@@ -1,4 +1,4 @@
-import { ref } from "vue"
+import { computed, ref, shallowRef } from "vue"
 
 document.addEventListener('mousemove', (event) => {
     if (event.target !== currentHoveredElement.value) {
@@ -7,3 +7,35 @@ document.addEventListener('mousemove', (event) => {
 })
 
 const currentHoveredElement = ref<HTMLElement>()
+
+const useHoverableElements = () => {
+    const hoverableElements = new Map<HTMLElement, boolean>()
+
+    const addHoverableElement = (element: HTMLElement, self: boolean = false) => {
+        hoverableElements.set(element, self)
+    }
+
+    const removeHoverableElement = (element: HTMLElement) => {
+        hoverableElements.delete(element)
+    }
+
+    const isCurrentElementHoverable = computed(() => {
+        if (typeof currentHoveredElement.value === 'undefined') {
+            return false
+        }
+
+        return [...hoverableElements]
+            .filter(([element, self]) => (self
+                ? element === currentHoveredElement.value
+                : element.contains(currentHoveredElement.value!)
+            )).length > 0
+    })
+
+    return {
+        isCurrentElementHoverable: shallowRef(isCurrentElementHoverable),
+        addHoverableElement,
+        removeHoverableElement,
+    }
+}
+
+export { useHoverableElements }
