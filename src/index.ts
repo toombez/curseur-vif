1e3fe2800f2b7fcbe5ff50cfc46a6246ba604fce
Timur <toombez.timurtokaev@gmail.com>
2024-10-28 23:27:51 +0300

    Export directives

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ import { useDynamicComponent } from "@/composables/useDynamicComponent";
 import { useHoverableElements } from "@/composables/useHoverableElements";
 import { useCursor } from "@/composables/useCursor";
 import { useCurseur } from "@/composables/useCurseur";
-import TheCursor from "@/components/TheCursor.vue";
-import curseurPlugin from "@/plugins/curseur";
 
-export type { HoverableElementData } from '@/composables/useHoverableElements'
+import TheCursor from "@/components/TheCursor.vue";
+
+import { vCursorDirective } from "@/directives/vCursor";
+import { vCursorHoverDirective } from "@/directives/vCursorHover";
+
+import curseurPlugin from "@/plugins/curseur";
 
 export {
     useDynamicComponent,
     useHoverableElements,
     useCursor,
     useCurseur,
+    vCursorDirective,
+    vCursorHoverDirective,
     TheCursor,
     curseurPlugin,
 }
