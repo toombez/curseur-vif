b4c5658b193e97f75c61f17e18b0a3ecdf0f2ec3
Timur <toombez.timurtokaev@gmail.com>
2024-10-28 23:32:45 +0300

    Add vite config library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+        dts({
+            tsconfigPath: './tsconfig.app.json',
+            rollupTypes: true
+        }),
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "CurseurVif",
+            fileName: "curseur-vif",
+            formats: ['es', 'umd'],
+        },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+        minify: true,
+        cssMinify: true,
+    },
 })
