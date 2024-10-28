6a6f7a094bb07da6a9d0ee0d5fd1cbd4ba6e9757
Timur <toombez.timurtokaev@gmail.com>
2024-10-28 21:00:09 +0300

    Add examples

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import BorderCursor from './components/BorderCursor.vue';
 import CircleCursor from './components/CircleCursor.vue';
+import DefaultCursor from './components/DefaultCursor.vue';
 import SquareCursor from './components/SquareCursor.vue';
 import TheCursor from './components/TheCursor.vue';
 </script>
@@ -22,15 +23,36 @@ import TheCursor from './components/TheCursor.vue';
             </div>
         </div>
 
-        <div style="width: 200px; aspect-ratio: 1; background-color: blue;"
-            v-cursor="{ attrs: {}, component: CircleCursor }"
-        ></div>
+        <div
+            :class="[$style.square, $style['outer-square']]"
+        >
 
-        <TheCursor />
+        </div>
+
+        <div
+            style="width: 200px; aspect-ratio: 1; background-color: blue;"
+            v-cursor="{ attrs: {}, component: CircleCursor }"
+        >
+        </div>
+
+        <div
+            style="width: 200px; aspect-ratio: 1; background-color: brown;"
+            :class="$style.square"
+            v-cursor-hover="'scale'"
+        >
+            <div style="width: 100px; aspect-ratio: 1; background-color: cadetblue"></div>
+        </div>
+
+        <TheCursor>
+            <DefaultCursor />
+        </TheCursor>
     </main>
 </template>
 
 <style module>
+html {
+    height: 1000vh;
+}
 .square {
     display: flex;
     justify-content: center;
---
+++ src/components/DefaultCursor.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useCurseur } from '@/composables/useCurseur';
+import { computed } from 'vue';
+
+const api = useCurseur()
+
+const isScale = computed(() => api.hoveredElementKey.value === 'scale')
+</script>
+
+<template>
+    <div :class="[$style.cursor, { [$style.scale]: isScale }]">
+    </div>
+</template>
+
+<style module>
+.cursor {
+    transition: scale 0.2s ease-in-out;
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    border: 1px solid black;
+}
+.scale {
+    scale: 4;
+}
+</style>
