3e30cfc2033a9d83b8e7f3a5a3e52b32483e776f
Timur <toombez.timurtokaev@gmail.com>
2024-10-28 12:26:04 +0300

    Register handlers using vcursor directive

--- src/directives/vCursor.ts
+++ src/directives/vCursor.ts
@@ -1,3 +1,4 @@
+import { useCurseur } from "@/composables/useCurseur"
 import type { AllComponentAttrs } from "@/composables/useDynamicComponent"
 import type { Component, Directive } from "vue"
 
@@ -14,3 +15,44 @@ type VCursorDirective = Directive<
     DirectiveValue,
     DirectiveModifiers
 >
+
+const vCursorDirective: VCursorDirective = {
+    mounted(el, binding) {
+        const { value, modifiers } = binding
+        const curseurApi = useCurseur()
+
+        let Component: Component = 'component' in value
+            ? value.component
+            : value
+
+        let attrs: Record<string, unknown> = 'attrs' in value
+            ? value.attrs
+            : {}
+
+        const mouseoverHandler = (event: MouseEvent) => {
+            if (modifiers.self && event.target !== el) {
+                return
+            }
+
+            curseurApi.setCursorComponent(Component, attrs)
+        }
+
+        const mouseleaveHandler = (event: MouseEvent) => {
+            if (modifiers.self && event.target !== el) {
+                return
+            }
+
+            curseurApi.deleteCursorComponent()
+        }
+
+        // element might be empty if page is just loaded
+        if (typeof el === 'undefined') {
+            return
+        }
+
+        el.addEventListener('mouseover', mouseoverHandler)
+        el.addEventListener('mouseleave', mouseleaveHandler)
+    }
+}
+
+export { vCursorDirective }
